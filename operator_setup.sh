253deb76f035ef6b467320507d5fd20a0f85baaa
Alin Spinu <spinualin@gmail.com>
2019-11-19 18:31:20 +0200

    update crds

--- operator_setup.sh
+++ operator_setup.sh
@@ -8,6 +8,7 @@ oc create -f ./deploy/crds/cache_v1alpha1_pvcmigration_cr.yaml
 oc create -f ./deploy/crds/cache_v1alpha1_postgresqlmigration_cr.yaml
 oc create -f ./deploy/crds/cache_v1alpha1_mongodbmigration_cr.yaml
 
+sleep 1
 
 echo "Creating Operator Resources"
 oc create -f ./deploy/service_account.yaml
@@ -19,8 +20,8 @@ oc create -f ./deploy/operator.yaml
 
 echo "Creating Operator RBAC"
 
-oc adm policy add-role-to-user admin jigger-operator -n ${NAMESPACE}
-oc adm policy add-cluster-role-to-user admin jigger-operator
-oc adm policy add-cluster-role-to-user cluster-admin jigger-operator 
+# oc adm policy add-role-to-user admin jigger-operator -n ${NAMESPACE}
+# oc adm policy add-cluster-role-to-user admin jigger-operator
+# oc adm policy add-cluster-role-to-user cluster-admin jigger-operator 
 
 echo "Exit"
